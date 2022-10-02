ec1495cca23eef52a68bb2f6ab21affee24c042d
Aleklz89 <108018670+Aleklz89@users.noreply.github.com>
2022-10-02 16:30:54 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace TRPZ2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+<<<<<<< HEAD
+            lb1.Items.Add("Stas Logvynskyi");
+=======
+            lb1.Items.Add("Oleksiy Okorokov");
+            lb1.Items.Add("Dmitrii Nieliepin");
+>>>>>>> 1f079e93a23091d1a82258dc942fdc6bbadd98de
+        }
+    }
+}
